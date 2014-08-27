1795ff1c37ba97534e78905695625de83bb2fd97
Thomas Iwaszko <tom@precision-T1500.(none)>
2014-08-27 12:21:06 +0200

    replacing the auto-quit by an auto-restart process, very minor adjustment of sound and music

--- EndGameMode.py
+++ EndGameMode.py
@@ -5,11 +5,12 @@ from pygame.locals import *
 from FontTool import *
 
 ( STEP_BAD_NEWS ) = range(1)
-( ACT_EXIT ) = range(1)
+( ACT_EXIT, ACT_RESTART ) = range(2)
 
 
 class EndGameMode( GameMode ):
     def __init__(self):
+        self.restart = False
         self.step = STEP_BAD_NEWS
         self.act=None
         self.model_stopped = False
@@ -18,8 +19,10 @@ class EndGameMode( GameMode ):
         for event in events:
             if event.type == QUIT:
                 self.act = ACT_EXIT
-            if(self.step==STEP_BAD_NEWS and event.type == pygame.MOUSEBUTTONDOWN):
-                self.act = ACT_EXIT
+
+            if(self.step==STEP_BAD_NEWS and (
+                 event.type == pygame.MOUSEBUTTONDOWN or event.type==pygame.KEYDOWN) ):
+                self.act = ACT_RESTART
 
     def update(self, gm_model):
 
@@ -31,6 +34,9 @@ class EndGameMode( GameMode ):
             gm_model.pause()
             self.model_stopped = True
 
+        if (self.act==ACT_RESTART):
+            self.restart = True
+
     def drawScreen(self, window, gm_model):
         y_border = generic_screen(window, gm_model)
         def_cause = gm_model.getDefeatCause()
@@ -39,4 +45,8 @@ class EndGameMode( GameMode ):
         blit_center(window, rpg_msg, (DISP_H/2)-(height_txt/2)  )
 
     def shouldChangeMode(self):
-        return False
+        return self.restart
+
+    def getNextMode(self):
+        from IntroMode import *
+        return IntroMode()
